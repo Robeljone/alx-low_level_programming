586da5b9081b132908f491c8600594fe8925d271
Robeljone <robelict94@gmail.com>
2022-03-10 19:14:56 +0300

    c is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 #include <string.h>
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
