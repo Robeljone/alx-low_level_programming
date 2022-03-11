f89bd5c62010e045d661d8095a4e7325183a95d9
Your Name <you@example.com>
2022-03-11 14:57:44 +0300

    commited

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+ *
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int lowercase,x;
+for (x = 'a'; x <= 'z'; x++)
+  {
+    lowercase = x;
+    if (lowercase != 'e' && lowercase != 'q')
+    {
+        putchar(lowercase);
+    }
+    else
+    {
+        
+    }
+  }
+return (0);
+}
