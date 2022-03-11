d8462aed6114897641e8fa86d1af9d14345c75f2
Your Name <you@example.com>
2022-03-11 14:05:50 +0300

    commited

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,15 +10,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
-if(lastdigit>5)
+if(lastdigit > 5)
 {
 printf("Last digit of = %d is = %d and is greater than 5", n,lastdigit);
 }
-else if (lastdigit==0)
+else if (lastdigit == 0)
 {
 printf("Last digit of = %d is = %d and is 0", n,lastdigit);
 }
-else if (lastdigit<6 && lastdigit>0)
+else if (lastdigit < 6 && lastdigit > 0)
 {
 printf("Last digit of = %d is = %d and is greater than 6 and not 0", n,lastdigit);
 }
