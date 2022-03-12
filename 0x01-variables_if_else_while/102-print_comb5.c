55315de71439b6bc3d532f74e479eb65c4e31c0b
Robeljone <robelict94@gmail.com>
2022-03-12 15:50:56 +0300

    commited

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,11 +8,14 @@ int main(void)
 int x;
 int y;
 int z;
+int w;
 for (x = '0'; x <= '9'; x++)
 {
 for (y = '0'; y <= '9'; y++)
 {
 for (z = '0'; z <= '9'; z++)
+{
+for (w = '0'; w <= '9'; w++)
 {
 if (x < y && y < z)
 {
@@ -23,6 +26,7 @@ if (!(x == '7' && y == '8' && z == '9'))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
