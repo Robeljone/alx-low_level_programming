14c0f60d8faff8901699477d3d3d5a6b8d6b1777
Robeljone <robelict94@gmail.com>
2022-03-15 19:45:56 +0300

    commited

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
-int main(void)
-{
-print_alpahbet();
-}
-int void print _alphabet(void)
+void print _alphabet(void)
 {
 char x='a';
 while(x != 'z')
 {
 putchar(x);
-putchar('\n');
 x++
 }
+putchar('\n');
 }
