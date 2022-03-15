58454a8c1039d9d38ff102b920a189dcda310a86
Your Name <you@example.com>
2022-03-15 15:17:47 +0300

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 int x;
 char ch[] = "_putchar";
-for (x=0; x < 8; x++)
+for (x = 0; x < 8; x++)
 {
 putchar(ch[x]);
 }
