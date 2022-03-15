a8121889d0121ec524d02d61c62420d7a6480fac
Robeljone <robelict94@gmail.com>
2022-03-15 20:07:30 +0300

    commited

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,12 +10,12 @@ int main(void)
 print_alphabet();
 return (0);
 }
-void print_alphabet()
+void print_alphabet(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-_putchar(ch);
+putchar(ch);
 }
-_putchar('\n');
+putchar('\n');
 }
