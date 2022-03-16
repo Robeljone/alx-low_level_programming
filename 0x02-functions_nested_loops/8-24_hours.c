8ae8ffe788c451f4ce3e4dcdc063d7e15eb409a2
Your Name <you@example.com>
2022-03-16 13:21:01 +0300

    commited

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,24 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* jack_bauer - does my stuff
+* @void: its mine damnit
+*
+* Return: Does my stuff explained.
+*/
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+int HourTen;
+int Minute;
+for (HourTen = 0; HourTen <= 23; HourTen++)
+{
+for (Minute = 0; Minute <= 59; Minute++)
+{
+_putchar('0' + (HourTen / 10));
+_putchar('0' + (HourTen % 10));
+_putchar(':');
+_putchar('0' + (Minute / 10));
+_putchar('0' + (Minute % 10));
+_putchar ('\n');
+}
+}
 }
