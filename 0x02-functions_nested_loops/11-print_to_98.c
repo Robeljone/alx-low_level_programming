b360025afc6e85d3937a624b805a5d82525cc82f
Your Name <you@example.com>
2022-03-16 14:05:20 +0300

    commited

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,50 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* jack_bauer - does my stuff
-* @void: its mine damnit
-*
-* Return: Does my stuff explained.
-*/
+ *print_to_98 - prints from n to 98
+ *
+ *Description: n to 98
+ *
+ *Return: void
+ *
+ *@n:input
+ */
 void print_to_98(int n)
 {
-while (n != 0)
-{
-_putchar(n);
-_putchar(',');
-_putchar(' ');
-}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n < 98)
+	{
+		int i;
+
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		int i;
+
+		for (i = n; i > 97; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
 }
