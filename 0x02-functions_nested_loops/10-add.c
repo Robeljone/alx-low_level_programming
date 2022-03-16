531f30a3d52974e1a69537a17de15bc646147623
Your Name <you@example.com>
2022-03-16 13:48:10 +0300

    commited

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* jack_bauer - does my stuff
+* @void: its mine damnit
+*
+* Return: Does my stuff explained.
+*/
+int add(int x, int y)
+{
+int sum;
+sum = x + y
+return (sum);
+}
