b7eb19ac8f24830172ac0f68fb86fe1a3a320dbb
Your Name <you@example.com>
2022-03-16 13:10:06 +0300

    commited

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,12 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
-
-int print_last_digit(int)
+int main(void)
 {
-int r;
-int m;
-m = print_last_digit(98);
-_putchar(m);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+    jack_bauer();
+    return (0);
 }
