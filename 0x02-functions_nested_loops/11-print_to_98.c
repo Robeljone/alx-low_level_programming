543aeabe3e69c68439ad5cb303650364c02d5cd6
Your Name <you@example.com>
2022-03-16 13:57:40 +0300

    commited

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* jack_bauer - does my stuff
+* @void: its mine damnit
+*
+* Return: Does my stuff explained.
+*/
+void print_to_98(int n)
+{
+int i;
+for (i = 0; i <= 97; i++)
+{
+_put(i);
+_putchar(',');
+_putchar(' ');
+}
+}
