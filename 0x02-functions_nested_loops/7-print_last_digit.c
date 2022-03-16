f23ac29b9c9336faff5308c5e3806bbea92a87e0
Your Name <you@example.com>
2022-03-16 10:47:50 +0300

    commited

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,16 +5,26 @@
  *
  * Return: Always 0.
  */
-int print_last_digit(int n)
+
+int main(void)
+
 {
-int r;
-int m;
-int x = n % 10;
-_putchar(x);
-m = print_last_digit(98);
-_putchar(m);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+
+  int r;
+  int m;
+  
+
+ m = print_last_digit(98);
+ _putchar(m);
+
+  print_last_digit(0);
+
+  r = print_last_digit(-1024);
+
+  _putchar('0' + r);
+
+  _putchar('\n');
+
+  return (0);
+
 }
