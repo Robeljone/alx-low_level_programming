1a0204bd1deb4dba1e8734f884697252861c568d
Your Name <you@example.com>
2022-03-16 08:52:12 +0300

    commited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
-* main - check the code.
+* print_alphabet_x10 - does my stuff
+* print the alphabet
 *
-* Return: Always 0.
+* ten times
 */
 void print_alphabet_x10(void)
 {
