076e8f665afd935ea4e7c40f6601ee365c9a0115
robel <robelict94@gmail.com>
2022-03-21 23:26:09 +0300

    comited

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int -change value using the pointer address
+*
+*@a: stores the input
+*@b: stores the input
+*/
+void swap_int(int *a, int *b)
+{
+int Tempa;
+int Tempb;
+Tempa = *a;
+Tempb = *b;
+*a=Tempb;
+*b=Tepa;
+}
