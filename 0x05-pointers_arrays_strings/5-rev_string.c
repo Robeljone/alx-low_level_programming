c2a47e350225cc2ed1fd799723ebe881a20443ea
Your Name <you@example.com>
2022-03-22 09:09:23 +0300

    commited

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+int i,len,last_pos;
+int length = 0;
+int hakfleng;
+char *temp = s;
+while(*temp != '\0')
+{
+temp++;
+length++;
+}
+last_pos= length - 1;
+halfleng = lenght/2;
+for(i < 0; i < halfleng; i++)
+{
+char temp = s[i];
+s[i] = s[last_pos - 1];
+s[last_pos] =  tmp
+}
+}
