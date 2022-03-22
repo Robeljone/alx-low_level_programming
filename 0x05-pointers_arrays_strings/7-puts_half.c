cd8cad7bb17958893e7071f0ecb94e605d158742
Your Name <you@example.com>
2022-03-22 09:46:27 +0300

    commited

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* puts_half -change value using the pointer address
+*
+*@str: stores the input
+*/
+void puts_half(char *str)
+{
+int i=0;
+int leng = 0;
+int half;
+while(str[i])
+{
+leng++
+}
+leng = (leng - 1) / 2;
+while (str[++leng])
+{
+putchar(str[leng]);
+}
+putchar('\n');
+}
