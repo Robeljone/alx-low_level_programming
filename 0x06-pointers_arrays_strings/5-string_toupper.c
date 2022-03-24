b465cefbae8356a742e1a2d45ccb1ffd6665b193
Your Name <you@example.com>
2022-03-24 10:17:49 +0300

    commited

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,11 @@ char *string_toupper(char *s)
 int c = 0;
 while (s[c] != '\0')
 {
-if (s[c] >= 'a' && s[c] <= 'z')
+if (s[c] >= 'a' && s[c] <= 'z' )
 {
 s[c] = s[c] - 32;
-}
 c++;
+}
 }
 return (s);
 }
