5d40445d560f51098c89e8a59543a3674c40c38d
Robeljone <robelict94@gmail.com>
2022-04-01 20:19:40 +0300

    comiited

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,18 @@
  */
 int main(int argc, char *argv[])
 {
-int num1;
-int num2;
-int total;
-if (argc < 3)
-{
-print ("%s\ln",Error);
-return (1);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-print ("%d\n",num1 * num2);
-return (0);
+	int num1;
+	int num2;
+
+	if (argc < 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
 }
