46b1c3f72d96d442d201e59b4f6b4c5e4bb57bf0
Robeljone <robelict94@gmail.com>
2022-04-01 20:02:35 +0300

    comiited

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: int
+ * @argv: array of strings
+ * Return: sum
+ */
+int main(int argc, char *argv[])
+{
+int num1;
+int num2;
+int total;
+if (argc < 0)
+{
+print ("%s\ln",Error);
+return (1);
+}
+total = num1 * num2;
+print ("%d\n", total);
+return (0);
+}
