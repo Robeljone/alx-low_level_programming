d03d26c7dcfb00972d99068554f080fcbb2d89f0
Your Name <you@example.com>
2022-04-04 11:28:11 +0300

    commited

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+* create_array - multiplies two numbers
+* @size: int
+* @c: array of strings
+*Return: pointer
+*/
 char *create_array(unsigned int size, char c)
 {
 char *str;
