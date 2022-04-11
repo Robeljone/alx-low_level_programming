b676d195b956d362c1c3b5f0800e72808d6eafa5
robel <robelict94@gmail.com>
2022-04-11 20:36:48 +0300

    commited

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct - define the  struct
+ *
+ * 
+ */
+struct dog
+{
+char name;
+float range;
+char owner
+}
+
+int main (void)
+{
+return 0;
+}
