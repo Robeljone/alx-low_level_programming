45d312bd80747564672290303f2975cd2d43815c
Your Name <you@example.com>
2022-06-14 08:34:54 +0300

    commited

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef HEADER_FILE
+#define HEADER_FILE
 #include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
